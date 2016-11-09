65eff638a6f62259e598f4ebada1b560785cb2c4
nonetheless <ggg66166161@163.com>
2016-11-09 21:37:07 +0800

    jdbc

--- homework/src/main/java/main/AppServlet.java
+++ homework/src/main/java/main/AppServlet.java
@@ -1,11 +1,20 @@
 package main;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 
 /**
  * Servlet implementation class AppServlet
@@ -13,26 +22,70 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/Hello")
 public class AppServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
-    /**
-     * Default constructor. 
-     */
-    public AppServlet() {
-        // TODO Auto-generated constructor stub
-    }
+	private DataSource datasource = null;
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * Default constructor.
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	public AppServlet() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void init() {
+		InitialContext jndiContext = null;
+
+		Properties properties = new Properties();
+		properties.put(javax.naming.Context.PROVIDER_URL, "jnp:///");
+		properties.put(javax.naming.Context.INITIAL_CONTEXT_FACTORY, "org.apache.naming.java.javaURLContextFactory");
+		try {
+			jndiContext = new InitialContext(properties);
+			datasource = (DataSource) jndiContext.lookup("java:comp/env/jdbc/test1");
+			System.out.println("got context");
+			System.out.println("About to get ds---Show");
+		} catch (NamingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		Connection connection = null;
+		PreparedStatement stmt = null;
+		String name = null;
+		ResultSet result = null;
+		try {
+			connection = datasource.getConnection();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		boolean isNull = false;
+		try {
+			stmt = connection.prepareStatement("select count(*) from t1;");
+			result = stmt.executeQuery();
+			while (result.next()) {
+				isNull = true;
+				break;
+			}
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		response.getWriter().append("Database Connection: ").append(String.valueOf(isNull));
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
